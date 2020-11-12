90f95a289461f0771db8c3c700f24f8446160ce5
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-11-11 20:41:51 -0600

    base mode auth complete

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user:', req.user);
-  pool
-    .query('SELECT * FROM "secret";')
-    .then((results) => res.send(results.rows))
-    .catch((error) => {
-      console.log('Error making SELECT for secrets:', error);
-      res.sendStatus(500);
-    });
+  console.log('/secrets GET route');
+  console.log('is authenticated?', req.isAuthenticated());
+  console.log('user', req.user);
+
+  // only allow authenticated users acces - franz
+  if(req.isAuthenticated() === false) {
+    res.sendStatus(403);
+  } else {
+    let queryText = `SELECT * FROM "secret" WHERE "secrecy_level" <= $1`;
+    pool
+      .query(queryText, [req.user.clearance_level])
+      .then((results) => res.send(results.rows))
+      .catch((error) => {
+        console.log('Error making SELECT for secrets:', error);
+        res.sendStatus(500);
+      });
+    }
 });
 
 module.exports = router;
